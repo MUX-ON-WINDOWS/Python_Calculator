37f2e94e9cc6624d40cfd23eec5a34c33cc587a9
Max <max.arnouts@ictmbo.nl>
2022-02-10 11:57:46 +0100

    Formatted file

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
 from cmath import pi
-import math # import math module
+import math  # import math module
+
+
 def add(num1, num2):
     return num1 + num2
+
+
 def subtract(num1, num2):
     return num1 - num2
+
+
 def multiply(num1, num2):
     return num1 * num2
+
+
 def divide(num1, num2):
     return num1 / num2
+
+
 def pical(num1, num2):
     return num1 * num2
 
-print("Pick calculation option: \n" \
-        "1. Add (+)\n" \
-        "2. Subtract (-)\n" \
-        "3. Multiply (*)\n" \
-        "4. Divide (/)\n" \
-        "5. Sqaure root (√)\n" \
-        "6. raised to the power (^)\n" \
-        "7. Calculated PI (π) \n")
 
-select = int(input("Select your calculation option: \n" \
-                    "1, 2, 3, 4, 5, 6, 7: \n"))
+print("Pick calculation option: \n"
+      "1. Add (+)\n"
+      "2. Subtract (-)\n"
+      "3. Multiply (*)\n"
+      "4. Divide (/)\n"
+      "5. Sqaure root (√)\n"
+      "6. raised to the power (^)\n"
+      "7. Calculated PI (π) \n")
 
+select = int(input("Select your calculation option: \n"
+                   "1, 2, 3, 4, 5, 6, 7: \n"))
 # Add
 if select == 1:
     number_1 = int(input("Enter first number: "))
@@ -51,14 +61,14 @@ elif select == 4:
 elif select == 5:
     number_1 = int(input("Enter your number: "))
     print("√", number_1, "=",
-                    math.sqrt(number_1))
+          math.sqrt(number_1))
 # Raised to the power
 elif select == 6:
     number_1 = int(input("Enter first number: "))
     number_2 = 2
     print(number_1, "^", number_2, "=",
                     math.pow(number_1, number_2))
-#Calculated with PI
+# Calculated with PI
 elif select == 7:
     number_1 = int(input("Enter your number: "))
     print(number_1, "*", "π", "=",
